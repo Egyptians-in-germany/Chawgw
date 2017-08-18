04b8faac7b215b07394b22113fa975b4f0659294
Mosab Ibrahim <mosab.a.ibrahim@gmail.com>
2017-08-18 13:56:39 +0200

    Formatted the files and changes the links

--- Chawgw.i18n.php
+++ Chawgw.i18n.php
@@ -9,14 +9,15 @@
 $messages = array();
 
 /** English
+ *
  * @author Nick White
  */
 $messages['en'] = array(
-	'skinname-chawgw' => 'Chawgw',
-	'chawgw-desc' => 'ڕووکارێکی سادەیە بۆ چاوگ ڕێکخراوە.',
-	'chawgw-skiptocontent' => 'Skip to content',
-	'chawgw-skiptosearch' => 'Skip to search',
-	'chawgw-action-addsection' => 'Add topic',
+	'skinname-chawgw'             => 'Chawgw',
+	'chawgw-desc'                 => 'ڕووکارێکی سادەیە بۆ چاوگ ڕێکخراوە.',
+	'chawgw-skiptocontent'        => 'Skip to content',
+	'chawgw-skiptosearch'         => 'Skip to search',
+	'chawgw-action-addsection'    => 'Add topic',
 
 	/* Extra content for footer columns. Leave these empty here,
 	 * they are intended to be customised by site administrators. */
@@ -26,12 +27,13 @@ $messages['en'] = array(
 );
 
 /** Message documentation
+ *
  * @author Nick White
  */
 $messages['qqq'] = array(
-	'skinname-chawgw' => 'Skin name',
-	'chawgw-desc' => '{{desc|name=Chawgw|url=http://www.chawg.org/wiki}}',
-	'chawgw-skiptocontent' => 'Link to skip directly to the content on the page',
-	'chawgw-skiptosearch' => 'Link to skip directly to the search box on the page',
+	'skinname-chawgw'          => 'Skin name',
+	'chawgw-desc'              => '{{desc|name=Chawgw|url=http://www.chawg.org/wiki}}',
+	'chawgw-skiptocontent'     => 'Link to skip directly to the content on the page',
+	'chawgw-skiptosearch'      => 'Link to skip directly to the search box on the page',
 	'chawgw-action-addsection' => 'Link to add a new section to talk page, overriding the default of "+"',
 );
